051d1a9b964bccb41494e02bd27bb8d69a0e875a
Glen Samuels <glen@liupierre.com>
2019-01-19 22:31:43 -0500

    initial commit

--- Final-Project/src/main/java/org/launchcode/FinalProject/FinalProjectApplication.java
+++ Final-Project/src/main/java/org/launchcode/FinalProject/FinalProjectApplication.java
@@ -1,13 +1,13 @@
 package org.launchcode.FinalProject;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FinalProjectApplication {
 
-	public static void main(String[] args) { SpringApplication.run(FinalProjectApplication.class, args);
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectApplication.class, args);
 	}
 
 }
